88d49d339b4b49d61e4b257ae0956db736a1ef7a
Erwin Wilbert Mapaliey <erwinmapaliey@gmail.com>
2024-10-05 20:02:41 +0800

    add basic layout & pages

---
+++ app/components/MenuItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul>
+    <li>
+      <NuxtLink to="#about">About</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="#experience">Experience</NuxtLink>
+    </li>
+    <li>
+      <NuxtLink to="#projects">Projects</NuxtLink>
+    </li>
+  </ul>
+</template>
---
+++ app/components/SocialMediaList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="flex gap-4">
+    <li>
+      <a href="#">Facebook</a>
+    </li>
+    <li>
+      <a href="#">Instagram</a>
+    </li>
+    <li>
+      <a href="#">LinkedIn</a>
+    </li>
+    <li>
+      <a href="#">GitHub</a>
+    </li>
+  </ul>
+</template>
